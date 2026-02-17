fee0ed7a768f1076c395fb1700c52961bc7225ad
Adiel A Daniel <adieladaniel@gmail.com>
2026-02-17 23:33:40 +0530

    Changes in db.js

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,11 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10
 });
-
-// console.log("DB_USER =", process.env.DB_USER);
-// console.log("DB_HOST =", process.env.DB_HOST);
-// console.log("DB_NAME =", process.env.DB_NAME);
-
